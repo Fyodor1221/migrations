2e42940be7b56d933b178284ca99c6ac93930282
Fyodor Parfyonov <parfyonov.fyodor@gmail.com>
2024-05-20 19:17:41 +0300

    migrations added

---
+++ src/main/resources/db.changelog/migrations/schema_v2.sql
@@ -0,0 +1,7 @@
+create table JDBC_DAO.PRODUCTS_V2 (
+                           id int auto_increment,
+                           name varchar(100),
+                           amount int,
+                           maker varchar(100),
+                           primary key (id)
+);
